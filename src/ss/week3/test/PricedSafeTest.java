8234fe4048f410808094c553f60b9804424dadc2
Legitizer <Legitizer@gmail.com>
2019-11-25 17:11:43 +0100

    added week3 files

---
+++ src/ss/week3/test/PricedSafeTest.java
@@ -0,0 +1,39 @@
+package ss.week3.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ss.week3.bill.Bill;
+import ss.week3.hotel.PricedSafe;
+
+public class PricedSafeTest {
+
+    private PricedSafe safe;
+    private static final double PRICE = 6.36;
+    private static final String PRICE_PATTERN = ".*6[.,]36.*";
+    
+    public String CORRECT_PASSWORD;
+    public String WRONG_PASSWORD;
+    
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        safe = new PricedSafe(PRICE);
+        CORRECT_PASSWORD = safe.getPassword().getInitPass();
+        WRONG_PASSWORD = CORRECT_PASSWORD + "WRONG";
+        assertFalse(safe.isActive());
+        assertFalse(safe.isOpen());
+    }
+    
+    @Test
+    public void testIsBillItem() throws Exception {
+    	assertTrue(safe instanceof Bill.Item, 
+    			"safe should be an instance of Bill.Item.");
+        assertEquals(PRICE, safe.getAmount(), 0, 
+        		"GetAmount should return the price of the safe.");
+    }
+}
